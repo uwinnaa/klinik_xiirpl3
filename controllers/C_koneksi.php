ab4ffc448aee8f92bb25137674effc3e28593bcc
uwinnaa <winanna675@gmail.com>
2023-08-10 14:16:39 +0700

    membuat fungsi koneksi kedalam database

--- controllers/C_koneksi.php
+++ controllers/C_koneksi.php
@@ -8,12 +8,25 @@ class C_koneksi{
     // ini ada fungsi atau method yang bernama connection dan fungsi harus ada didalam kelas 
     public function connection(){
         // untuk isinya kita lanjutkan hari rabu 
+
+        // membuat fungsi untuk terkoneksi kedalam database klinik_xiirpl3
+        $conn = mysqli_connect('localhost','root','','klinik_xiirpl3');
+        
+        if (!$conn) {
+            die("koneksi gagal dibuat : ".mysql_connect_error());
+        }else{
+            echo "koneksi berhasil dibuat";
+        }
+
+
     }
 
 }
 
-
-
+//inisialisasi objek
+$koneksi = new C_koneksi();
+//memanggil method atau fungsi yang ada didalam kelas c_koneksi
+$koneksi->connection();
 ?>
 
 
