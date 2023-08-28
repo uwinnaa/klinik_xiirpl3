8b98219c652eeff6ea78cfca12dd3a39dce08894
uwinnaa <winanna675@gmail.com>
2023-08-28 09:26:57 +0700

    mengupload tugas kemaren

--- controllers/C_koneksi.php
+++ controllers/C_koneksi.php
@@ -6,27 +6,32 @@
 class C_koneksi{
 
     // ini ada fungsi atau method yang bernama connection dan fungsi harus ada didalam kelas 
-    public function connection(){
-        // untuk isinya kita lanjutkan hari rabu 
+    public function conn(){ 
 
         // membuat fungsi untuk terkoneksi kedalam database klinik_xiirpl3
-        $conn = mysqli_connect('localhost','root','','klinik_xiirpl3');
-        
+        $conn = mysqli_connect('localhost','root','','klinik_xiipl3');
+
+        //untuk mengecek apakah koneksi berhasil dibuat atau tidak
         if (!$conn) {
-            die("koneksi gagal dibuat : ".mysql_connect_error());
+            die("koneksi gagal dibuat : ".mysqli_connect_error());
         }else{
-            echo "koneksi berhasil dibuat";
+            // echo "koneksi berhasil";
+            //untuk mengembalikan nilai koneksi
+            return $conn;
         }
 
 
     }
 
-}
 
+
+
+}
 //inisialisasi objek,harus diluar kelas
-$koneksi = new C_koneksi();
+// $koneksi = new C_koneksi();
 //memanggil method atau fungsi yang ada didalam kelas c_koneksi
-$koneksi->connection();
+// $koneksi->conn();
+
 ?>
 
 
---
+++ controllers/c_login.php
@@ -0,0 +1,108 @@
+<?php
+
+//untuk menhubungkan file c koneksi kedalam c login
+    include_once 'C_koneksi.php';
+
+//membuat class login
+class C_login{
+
+    //membuat fungsi untuk menangani registrasi user
+    //parameter adalah nilai yang diberikan kepada sebuah fungsi,metode,atau prosedur sebagai masukan untuk mengatur atau mengubah prilaku dari tindakan yang dijalankan oleh fungsi tersebut.
+    public function register($id=0, $nama=null, $email=null, $pass=null, $role=null){
+    
+        //membuat sebuah variabel yang bertipe data objek dari kelas/ file C_koneksi
+        $koneksi = new C_koneksi();
+
+        $sql = "INSERT INTO user VALUES ('$id','$nama','$email','$pass','$role','')";
+
+
+        //$sql = "INSERT INTO user VALUES ('$id', '$nama', '$email', '$pass', '$role','')";
+        
+        $query = mysqli_query($koneksi->conn(), $sql); // -> true/false
+
+        if ($query) {
+            echo "Data berhasil di tambahkan";
+        } else {
+            echo "Data gagal di tambahkan";
+        }
+        
+        }
+        
+        
+
+    //membuat fungsi untuk login user
+    public function login($email, $pass){
+
+        $conn = new C_koneksi();
+
+
+        //untuk mengecek tombol login sudah ditekan atau di klik oleh user
+        if (isset ($_POST['login'])) {
+
+            //untuk menampilkan sema data dari table user berdasarkan email yang yag diinputkan oleh user
+            $sql = "SELECT * FROM user WHERE email = '$email'";
+
+            $query = mysqli_query($conn->conn(), $sql);
+
+            //mengubah data dari bertipe data objek menjadi array asosiatif
+            $data = mysqli_fetch_assoc($query);
+            //untuk mengecek apakah ada data dari hasil query
+            if ($data) {
+                
+                //untuk mengecek atau membandingkan intupan password dari tabel user dengan password dari tabel user
+                if (password_verify($pass,$data['password'])) {
+                    
+                    //untuk mengecek apakah posisi login sebagai admin, untuk mengecek apakah role user itu sebagai admin atau bukan
+                    if($data['role'] == 'admin') {
+                        
+                        //untuk menampug data dari query database yang akan digunakan ketika halaman admin/user setelah proses login berhasil
+                        $_SESSION['data'] = $data;
+                        $_SESSION['role'] = $data['role'];
+
+                        //memindahkan halaman ke halaman home
+                        header("Location: ../views/home.php");
+                        exit;
+
+
+
+                    //untuk mengecek apakah posisi login sebagai user,untuk mengecek apakah role user iu sebagai user atau bukan
+                    } elseif ($data['role'] == 'user') {
+
+                        $_SESSION['data'] = $data;
+                        $_SESSION['role'] = $data['role'];
+
+
+                        header("Location: ../views/home_user.php");
+                        exit;
+                        
+                    }
+                    
+                
+                }else {
+                    echo "cek password";
+                }
+
+
+            }else {
+                echo "cek username dan password";
+            }
+            
+
+
+        }
+
+        }
+        
+
+    }
+    
+    //membuat fungsi untuk logout user
+    public function logout(){
+
+    }
+
+
+
+
+
+?>
